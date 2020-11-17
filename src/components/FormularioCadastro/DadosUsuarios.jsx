43146e767ee1fde36c2c01bc96318cb637da523a
Thiago <thiago.kamada@gmail.com>
2020-11-17 16:45:14 +0900

    Incluindo multiplas telas

--- src/components/FormularioCadastro/DadosUsuarios.jsx
+++ src/components/FormularioCadastro/DadosUsuarios.jsx
@@ -1,14 +1,19 @@
 import { Button, TextField } from "@material-ui/core";
 import React from "react";
 
-function DadosUsuario() {
+function DadosUsuario({aoEnviar}) {
     return (
-        <form>
+        <form onSubmit={(event)=>{
+            event.preventDefault();
+            aoEnviar();
+        }}>
             <TextField id="email" label="email" type="email" variant="outlined"
                 margin="normal"
+                required
                 fullWidth />
             <TextField id="senha" label="senha" type="password" variant="outlined"
                 margin="normal"
+                required
                 fullWidth />
             <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
         </form>
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,16 +2,39 @@ import React from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuarios";
 import DadosEntrega from "./DadosEntrega";
+import { useState } from "react";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(0);
 
-  return (
-    <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-      <DadosUsuario />
-      <DadosEntrega />
-    </>
-  );
+  const formularios = [<DadosUsuario aoEnviar={proximo} />,
+  <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+  <DadosEntrega aoEnviar={aoEnviar} />
+  ];
+
+function proximo() {
+  setEtapaAtual(etapaAtual + 1);
 }
+
+// function formularioAtual(etapa) {
+//   switch (etapa) {
+//     case 0:
+//       return <DadosUsuario aoEnviar={proximo} />;
+//     case 1:
+//       return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />;
+//     case 2:
+//       return <DadosEntrega aoEnviar={aoEnviar} />;
+//     default:
+//       return <Typography>Erro ao selecionar formulario</Typography>
+//   }
+// }
+
+return (
+  <>
+    {formularios[etapaAtual]}
+  </>
+);
+}
+
 
 export default FormularioCadastro;
